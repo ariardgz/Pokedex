e85b562bbc7eb57b77efbc2473f154d5e594debb
Your Name <you@example.com>
2023-08-16 19:02:36 -0500

    Código para visualizar lso datos importados

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import { example } from './data.js';
+import pokemon from './data/pokemon/pokemon.js';
 
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(["pokemon"][0]["name"]);
+console.log(data.pokemon[0].name);
+//elegir con un selector 
+const nombrepokemon = document.querySelector("#nombrepokemon");
+nombrepokemon.innerHTML=(data.pokemon[0].name);
+console.log(document.querySelector("#nombrepokemon"));
+
+
