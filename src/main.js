b96d53dcd24a2235c044df828481fa217af2d638
Ariadna Rodriguez <ariadna.rodriguez30@hotmail.com>
2023-08-17 11:41:30 -0600

    avance

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
+import { Linter } from 'eslint';
 import { example } from './data.js';
 
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(["pokemon"][0]["name"]);
+//console.log(data["pokemon"][0]["name"]);
+const imagenPokemon = data.pokemon[1].img;
+document.getElementById("#imagenPokemon.src") = imagenPokemon;
