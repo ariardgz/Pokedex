cf54eb05e2dc7439cf78467a43bfc20697582672
Ariadna Rodriguez <ariadna.rodriguez30@hotmail.com>
2023-08-17 18:08:22 -0600

    Agregando encabezado responsivo

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { Linter } from 'eslint';
+
 import { example } from './data.js';
 import pokemon from './data/pokemon/pokemon.js';
 
@@ -7,13 +7,24 @@ import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 //console.log(data["pokemon"][0]["name"]);
-const imagenPokemon = data.pokemon[1].img;
-//document.getElementById("#imagenPokemon.src") = imagenPokemon;
-console.log(data.pokemon[2].name);
+//const imagenPokemon = document.querySelector("src")
+//imagenPokemon.innerHTML = data.pokemon[0].img;
+
+
+
+//console.log(data.pokemon[2].name);
 //elegir con un selector 
-const nombrepokemon = document.querySelector("#nombrepokemon");
-nombrepokemon.innerHTML=(data.pokemon[2].name);
-console.log(document.querySelector("#nombrepokemon"));
+
+const nombrePokemon = document.querySelector("#nombrepokemon");
+nombrePokemon.innerHTML=(data.pokemon[0].name);
+//console.log(document.querySelector("#nombrepokemon"));
+
+const descripcionPokemon = document.querySelector("#descripcionPokemon");
+descripcionPokemon.innerHTML = (data.pokemon[0].about);
+
+const tipoPokemon = document.querySelector("#tipoPokemon");
+tipoPokemon.innerHTML = (data.pokemon[0].type[0]);
+
 
 
 
