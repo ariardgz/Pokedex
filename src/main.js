28fff4642e8fcaf4c00ef2f535842bd25f58b2d1
Ariadna Rodriguez <ariadna.rodriguez30@hotmail.com>
2023-08-21 09:52:27 -0600

    Se agrego diseño CSS para la pagina de inicio y pokedex

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 
-import { example } from './data.js';
-import pokemon from './data/pokemon/pokemon.js';
+//import { example } from './data.js';
+
 
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
@@ -15,7 +15,7 @@ import data from './data/pokemon/pokemon.js';
 //console.log(data.pokemon[2].name);
 //elegir con un selector 
 
-const nombrePokemon = document.querySelector("#nombrepokemon");
+const nombrePokemon = document.querySelector("#nombrePokemon");
 nombrePokemon.innerHTML=(data.pokemon[0].name);
 //console.log(document.querySelector("#nombrepokemon"));
 
@@ -24,6 +24,13 @@ descripcionPokemon.innerHTML = (data.pokemon[0].about);
 
 const tipoPokemon = document.querySelector("#tipoPokemon");
 tipoPokemon.innerHTML = (data.pokemon[0].type[0]);
+
+const numeroPokemon = document.querySelector("#numeroPokemon");
+numeroPokemon.innerHTML = (data.pokemon[0].num);
+
+const imagenPokemon = document.getElementById("imagenPokemon").src;
+imagenPokemon.innerHTML = (data.pokemon[0].img);
+
 
 
 
