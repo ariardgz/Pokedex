00e723f272fbee9dc6396931add125efa031ccc5
Ariadna Rodriguez <ariadna.rodriguez30@hotmail.com>
2023-09-04 15:35:23 -0600

    Se realiza el primer test para tipo pokemon

--- src/main.js
+++ src/main.js
@@ -1,14 +1,74 @@
-import { example } from './data.js';
+import { filterByType } from './data.js';
 import pokemon from './data/pokemon/pokemon.js';
 
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+//FILTRAR POR NOMBRE ASCENDENTE A-Z
+
+const pokemonNombres = (data.pokemon.map(pokemon => pokemon.name));
+const botonOrderAZ = document.querySelector('#orderAZ');
+botonOrderAZ.addEventListener('click', OrdernarPokemonAZ);
+pokemonNombres.sort();
+function OrdernarPokemonAZ(){
+  console.log(pokemonNombres);
+  pokemonDetailsContainer.innerHTML = "";
+  for(let i= 0; i < data.pokemon.length; i++ ){
+    if(pokemonNombre.[i]===data.pokemon[i].name){
+
+    }
+
+  }
+  
+  pokemonNombres.forEach((pokemon) => {
+    
+
+    /*const pokemonDiv = document.createElement("div");
+    pokemonDiv.classList.add('card');
+    const imagenPokemon = document.createElement("img");
+    imagenPokemon.src = pokemon.img;
+    imagenPokemon.alt = pokemon.name;
+    imagenPokemon.classList.add("img")
+    pokemonDiv.appendChild(imagenPokemon);
+
+    const nombrepokemon = document.createElement("h2");
+    nombrepokemon.textContent = "Name: " + pokemon.name;
+    pokemonDiv.appendChild(nombrepokemon);
+
+    const numeropokemon = document.createElement("p");
+    numeropokemon.textContent = "#" + pokemon.num;
+    pokemonDiv.appendChild(numeropokemon);
+
+    const generacionpokemon = document.createElement("p");
+    generacionpokemon.textContent = "Generation: " + pokemon.generation.name;
+    pokemonDiv.appendChild(generacionpokemon);
+
+    const tipopokemon = document.createElement("p");
+    tipopokemon.textContent = "Type: " + pokemon.type.join(", ");
+    pokemonDiv.appendChild(tipopokemon);
+
+    const alturapokemon = document.createElement("p");
+    alturapokemon.textContent = "Height: " + pokemon.size.height;
+    pokemonDiv.appendChild(alturapokemon);
+
+    const pesopokemon = document.createElement("p");
+    pesopokemon.textContent = "Weight: " + pokemon.size.weight;
+    pokemonDiv.appendChild(pesopokemon);
+
+    const descripcionpokemon = document.createElement("p");
+    descripcionpokemon.textContent = "About: " + pokemon.about;
+    pokemonDiv.appendChild(descripcionpokemon);
+    // Agrega más elementos aquí para otros detalles
+    pokemonDetailsContainer.appendChild(pokemonDiv);
+*/
+  })
+}
+
 
 ////////////////////FILTRADO POR NOMBRE
 const buscarPokemon = document.querySelector('#pokemon');
 const boton = document.querySelector('#botonSearch');
-
+const pokemonDetailsContainer = document.querySelector("#pokemon-details");
 buscarPokemon.addEventListener("keyup", function () { this.value = this.value.toLowerCase() })
 boton.addEventListener('click', searchPokemon);
 
@@ -64,12 +124,9 @@ function searchPokemon() {
       pokemonDetailsContainer.innerHTML = "Pokémon no encontrado";
     }
 
+
   }
 }
-
-
-
-
 
 ////////////////////////FILTRADO POR TIPO
 const botones = document.querySelectorAll(".btn-header");
@@ -77,8 +134,11 @@ botones.forEach(boton => boton.addEventListener('click', (event) => {
 
   const botonID = event.currentTarget.id;
   pokemonDetailsContainer.innerHTML = "";
-  const pokemonTipos = data.pokemon.filter(pokemon => pokemon.type.includes(botonID));
+  const pokemonTipos = filterByType(data, botonID) // argumentos (valor real)
 
+  if (botonID === 'all') {
+    return mostrarTodaLaData();
+  }
 
   pokemonTipos.forEach((pokemon) => {
     const pokemonDiv = document.createElement("div");
@@ -126,49 +186,50 @@ botones.forEach(boton => boton.addEventListener('click', (event) => {
 
 ))
 /////////TODOS LOS POKEMONES 
+function mostrarTodaLaData() {
+  data.pokemon.forEach((pokemon) => {
+    const pokemonDiv = document.createElement("div");
+    pokemonDiv.classList.add('card');
+
+    const imagenPokemon = document.createElement("img");
+    imagenPokemon.src = pokemon.img;
+    imagenPokemon.alt = pokemon.name;
+    imagenPokemon.classList.add("img")
+    pokemonDiv.appendChild(imagenPokemon);
+
+    const nombrepokemon = document.createElement("h2");
+    nombrepokemon.textContent = "Name: " + pokemon.name;
+    pokemonDiv.appendChild(nombrepokemon);
+
+    const numeropokemon = document.createElement("p");
+    numeropokemon.textContent = "#" + pokemon.num;
+    pokemonDiv.appendChild(numeropokemon);
+
+    const generacionpokemon = document.createElement("p");
+    generacionpokemon.textContent = "Generation: " + pokemon.generation.name;
+    pokemonDiv.appendChild(generacionpokemon);
+
+    const tipopokemon = document.createElement("p");
+    tipopokemon.textContent = "Type: " + pokemon.type.join(", ");
+    pokemonDiv.appendChild(tipopokemon);
+
+    const alturapokemon = document.createElement("p");
+    alturapokemon.textContent = "Height: " + pokemon.size.height;
+    pokemonDiv.appendChild(alturapokemon);
+
+    const pesopokemon = document.createElement("p");
+    pesopokemon.textContent = "Weight: " + pokemon.size.weight;
+    pokemonDiv.appendChild(pesopokemon);
+
+    const descripcionpokemon = document.createElement("p");
+    descripcionpokemon.textContent = "About: " + pokemon.about;
+    pokemonDiv.appendChild(descripcionpokemon);
+    // Agrega más elementos aquí para otros detalles
+    pokemonDetailsContainer.appendChild(pokemonDiv);
 
 
-const pokemonDetailsContainer = document.querySelector("#pokemon-details");
-data.pokemon.forEach((pokemon) => {
-  const pokemonDiv = document.createElement("div");
-  pokemonDiv.classList.add('card');
-  
-  const imagenPokemon = document.createElement("img");
-  imagenPokemon.src = pokemon.img;
-  imagenPokemon.alt = pokemon.name;
-  imagenPokemon.classList.add("img")
-  pokemonDiv.appendChild(imagenPokemon);
+  })
 
-  const nombrepokemon = document.createElement("h2");
-  nombrepokemon.textContent = "Name: " + pokemon.name;
-  pokemonDiv.appendChild(nombrepokemon);
-
-  const numeropokemon = document.createElement("p");
-  numeropokemon.textContent = "#" + pokemon.num;
-  pokemonDiv.appendChild(numeropokemon);
-
-  const generacionpokemon = document.createElement("p");
-  generacionpokemon.textContent = "Generation: " + pokemon.generation.name;
-  pokemonDiv.appendChild(generacionpokemon);
-
-  const tipopokemon = document.createElement("p");
-  tipopokemon.textContent = "Type: " + pokemon.type.join(", ");
-  pokemonDiv.appendChild(tipopokemon);
-
-  const alturapokemon = document.createElement("p");
-  alturapokemon.textContent = "Height: " + pokemon.size.height;
-  pokemonDiv.appendChild(alturapokemon);
-
-  const pesopokemon = document.createElement("p");
-  pesopokemon.textContent = "Weight: " + pokemon.size.weight;
-  pokemonDiv.appendChild(pesopokemon);
-
-  const descripcionpokemon = document.createElement("p");
-  descripcionpokemon.textContent = "About: " + pokemon.about;
-  pokemonDiv.appendChild(descripcionpokemon);
-  // Agrega más elementos aquí para otros detalles
-  pokemonDetailsContainer.appendChild(pokemonDiv);
-
-
-})
+}
+mostrarTodaLaData();
 
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,48 @@
-import { example, anotherExample } from '../src/data.js';
+import { filterByType } from '../src/data.js';
 
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+describe('filterByType ', () => {
+  // existe
+  it('deberia ser una funcion', () => {
+    expect(typeof filterByType).toBe('function');
   });
-
+  // comportamiento deseado
   it('returns `example`', () => {
-    expect(example()).toBe('example');
+    const botonID = "dark";
+    const data = {
+      "pokemon": [
+        { "name": "wartortle", "type": "dark" },
+        { "name": "pikachu", "type": "electric" },
+        { "name": "blastoise", "type": "water" }
+      ]
+    }
+    // console.log(filterByType(data, botonID));
+    expect(filterByType(data, botonID)).toEqual([{ "name": "wartortle", "type": "dark" }]);
   });
+  // estresar al test (AAA)
+  it('estresar', () => {
+    const botonID = "";
+    const data = {
+      "pokemon": [
+        { "name": "wartortle", "type": "dark" },
+        { "name": "pikachu", "type": "electric" },
+        { "name": "blastoise", "type": "water" }
+      ]
+    }
+    // console.log(filterByType(data, botonID));
+    expect(filterByType(data, botonID)).toBe(false)
+    expect(filterByType(data, botonID)).toBeFalsy()
+
+  })
 });
 
 
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
+// describe('anotherExample', () => {
+//   it('is a function', () => {
+//     expect(typeof anotherExample).toBe('function');
+//   });
 
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
-  });
-});
+//   it('returns `anotherExample`', () => {
+//     expect(anotherExample()).toBe('OMG');
+//   });
+// });
