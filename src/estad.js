2e85244c2239eac64addfb54b86cd9dcbb2a10f9
Ariadna Rodriguez <ariadna.rodriguez30@hotmail.com>
2023-09-10 17:57:23 -0600

    Arreglando footer

--- src/estad.js
+++ src/estad.js
@@ -4,35 +4,6 @@ import data from './data/pokemon/pokemon.js';
 import {tiposDePokemonKanto} from './data.js'
 import {tiposDePokemonJohto} from './data.js'
 //const graphic = document.getElementById("myChard")
-
-const tiposPoke = ["normal", "fire", "water", "grass", "electric", "ice", "fighting", "poison", "ground", "flying", "psychic", "bug", "rock", "ghost", "dark",
-  "dragon", "steel", "fairy"];
-//const tiposPoke = ["grass"];
-//const kantograss = []
-const obj = {}
-const obj2 = {}
-for (let i = 0; i < tiposPoke.length; i++) {
-
-  // console.log(tiposPoke[i], data.pokemon);
-  const pokemonKantoTipos = data.pokemon.filter(pokemon => {
-    if (pokemon.type.includes(tiposPoke[i]) && pokemon.generation.name.includes('kanto')) {
-      return pokemon;
-    }
-  }).length;
-  const pokemonJohtoTipos = data.pokemon.filter(pokemon => {
-    if (pokemon.type.includes(tiposPoke[i]) && pokemon.generation.name.includes('johto')) {
-      return pokemon;
-    }
-  }).length;
-
-
-  obj[tiposPoke[i]] = pokemonKantoTipos;
-  
-  obj2[tiposPoke[i]] = pokemonJohtoTipos;
-}
-
-console.log(obj);
-console.log(tiposDePokemonKanto(data));
 const barColor = "#705898"
 
 const xKantoValue = Object.keys(tiposDePokemonKanto(data));
